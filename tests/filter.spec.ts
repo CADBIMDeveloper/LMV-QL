14da35181603dadd0269ada8d09dd738a7b0c0b2
Alexander Ignatovich <cadbimdeveloper@yandex.ru>
2023-02-04 22:50:06 +0200

    add new tests

--- tests/filter.spec.ts
+++ tests/filter.spec.ts
@@ -19,7 +19,12 @@ describe("Filter tests", () => {
             numberValue1: 5.7,
             numberValue2: 1.3,
             stringValue: "test"
-        }, ["Category2", "Subcategory3"])
+        }, ["Category2", "Subcategory3"]),
+        new SimpleFilterableElement({
+            numberValue1: 5.7,
+            numberValue2: 1.3,
+            stringValue: "test"
+        }, ["Category1", "Subcategory1", "SubSub2"])
     ];
 
     const filterFactory = new FilterFactory();
@@ -30,5 +35,23 @@ describe("Filter tests", () => {
         assert.isTrue(filter(sourceElements[0]));
         assert.isTrue(filter(sourceElements[1]));
         assert.isFalse(filter(sourceElements[2]));
+    });
+
+    it("must filter for sub category", () => {
+        const filter = filterFactory.createFilter("[Category1].Subcategory1!");
+
+        assert.isTrue(filter(sourceElements[0]));
+        assert.isFalse(filter(sourceElements[1]));
+        assert.isFalse(filter(sourceElements[2]));
+        assert.isTrue(filter(sourceElements[3]));
+    });
+
+    it("must filter for sub-sub category", () => {
+        const filter = filterFactory.createFilter("[Category1].Subcategory1.SubSub1!");
+
+        assert.isTrue(filter(sourceElements[0]));
+        assert.isFalse(filter(sourceElements[1]));
+        assert.isFalse(filter(sourceElements[2]));
+        assert.isFalse(filter(sourceElements[3]));
     });
 });
