d21822d2013c3a68260ea65c8543282dc8df9ae2
Alexander Ignatovich <cadbimdeveloper@yandex.ru>
2023-02-04 21:29:41 +0200

    define a new operation

--- src/filterOperations.ts
+++ src/filterOperations.ts
@@ -3,6 +3,21 @@ import { FilterActionDict } from "./filtergrammar.ohm-bundle";
 
 export type Filter = (element: IFilterableElement) => boolean;
 
+type Category = {
+    type: "exact-category";
+    categories: string[];
+}
+
+type Property = {
+    type: "property-value";
+    propertyName: string;
+    categories: string[];
+}
+
 export const compile: FilterActionDict<Filter> = {
+
+}
+
+export const getPropertyDefinition: FilterActionDict<Category | Property> = {
 
 }
