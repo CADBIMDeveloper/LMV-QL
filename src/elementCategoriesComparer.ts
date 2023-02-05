1f6792f9f1d38f4d8e152eea0566925c485f28fa
Alexander Ignatovich <cadbimdeveloper@yandex.ru>
2023-02-05 16:04:48 +0200

    expand templates when comparing categories

--- src/elementCategoriesComparer.ts
+++ src/elementCategoriesComparer.ts
@@ -1,11 +1,23 @@
+import { expandTemplateCategories } from "./expandedWildcategoriesFactory";
+
 export const compareCategories = (elementCategories: string[], templateCategories: string[]): boolean => {
     if (templateCategories.length > elementCategories.length)
         return false;
 
-    for (let i = 0; i < templateCategories.length; i++) {
-        if (elementCategories[i] !== templateCategories[i])
-            return false;
+    const expandedTemplates = expandTemplateCategories(templateCategories, elementCategories.length);
 
+    for (const template of expandedTemplates)
+        if (validate(elementCategories, template))
+            return true;
+
+    return false;
+}
+
+const validate = (elementCategories: string[], templateCategories: string[]) => {
+    for (let i = 0; i < templateCategories.length; i++) {
+        if (templateCategories[i] !== "*" && elementCategories[i] !== templateCategories[i])
+            return false;
     }
+
     return true;
 }
