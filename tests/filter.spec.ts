6e96c59d28b1f139e8a6dfc3235af5ba4f2fc6f3
Alexander Ignatovich <cadbimdeveloper@yandex.ru>
2023-02-05 11:25:10 +0200

    add a new test

--- tests/filter.spec.ts
+++ tests/filter.spec.ts
@@ -104,4 +104,13 @@ describe("Filter tests", () => {
         assert.isFalse(filter(new SimpleFilterableElement({ property: "abc" }, ["Category"])));
         assert.isFalse(filter(new SimpleFilterableElement({ }, ["Category"])));
     });
+
+    it("must filter for test property equality", () => {
+        const filter = filterFactory.createFilter("Category.property = \"test\"");
+
+        assert.isTrue(filter(new SimpleFilterableElement({ property: "test" }, ["Category"])));
+        assert.isFalse(filter(new SimpleFilterableElement({ property: "abc" }, ["Other Category"])));
+        assert.isFalse(filter(new SimpleFilterableElement({ property: 5.7 }, ["Category"])));
+        assert.isFalse(filter(new SimpleFilterableElement({ }, ["Category"])));
+    });
 });
