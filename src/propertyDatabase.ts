6b22cf3b1c4cb2b9ae54e5111089d74ac855225e
Alexander Ignatovich <cadbimdeveloper@yandex.ru>
2023-02-11 11:52:26 +0200

    type definitions of property database from Forge viewer

---
+++ src/propertyDatabase.ts
@@ -0,0 +1,33 @@
+export type PropertyDatabase = {
+    findRootNodes(): number[];
+
+    findParent(dbId: number): number | null;
+
+    enumAttributes(callBack: (attrId: number, attrDef: AttributeDefinition) => void): void;
+
+    enumObjects(callBack: (dbId: number, fromId?: number, toId?: number) => void): void;
+
+    enumObjectProperties(dbId: number, callBack: (attrId: number, attrValueId: number) => void): void;
+
+    getNodeNameAndChildren(query: { dbId: number }): { dbId: number, parent: number };
+
+    _getObjectProperty(attrId: number, attrValueId: number): PropertyValue;
+}
+
+export type AttributeDefinition = {
+    category: string;
+    name: string;
+    dataType: number;
+    displayName: string | null;
+}
+
+export type PropertyValue = {
+    attributeName: string;
+    displayCategory: string | null;
+    displayName: string;
+    displayValue: string | number;
+    hidden: boolean;
+    precision: number;
+    type: number;
+    units: string | null
+}
