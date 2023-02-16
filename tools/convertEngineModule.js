0e038c0b702c9c0f52e8096ed6d84d6c961a9060
Alexander Ignatovich <cadbimdeveloper@yandex.ru>
2023-02-16 12:14:23 +0200

    compile engine into string

--- tools/convertEngineModule.js
+++ tools/convertEngineModule.js
@@ -1,10 +1,19 @@
 const fs = require("fs");
-//D:\Projects\LMV-QL\build\obj\engine.js
-fs.readFile("./build/obj/engine.js", {}, (_err, buf) => {
-    const engine = buf.toString()
-        .replace(
-            ";})();\n",
-            ";});\n\nmodule.exports = { engine };\n");
 
-    fs.writeFile("./engine.js", engine, () => { });
+const compile = (code) => {
+    const source = code.replace(";})();\n", ";});");
+
+    const engine = eval(`${source} engine`).toString();
+
+    const engineRegex = /^{\"engine\":\"(?<engine>.*)\"}$/;
+
+    const engineMatch = JSON.stringify({ engine }).match(engineRegex);
+
+    return `export const engine = \"${engineMatch.groups["engine"]}\";`;
+}
+
+fs.readFile("./build/obj/engine.js", {}, (_err, buf) => {
+    const code = buf.toString();
+
+    fs.writeFile("./engine.ts", compile(code), () => { });
 });
