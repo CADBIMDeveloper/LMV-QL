0f28313e566bceddf50930af80deaaeb90d3ff8a
Alexander Ignatovich <cadbimdeveloper@yandex.ru>
2023-02-17 18:57:08 +0200

    store attributes map (by integer attribute id)

--- src/propertyDatabaseAttributesCollection.ts
+++ src/propertyDatabaseAttributesCollection.ts
@@ -1,14 +1,17 @@
-import { PropertyDatabase } from "../propertyDatabase";
+import { AttributeDefinition, PropertyDatabase } from "../propertyDatabase";
 
 export class PropertyDatabaseAttributesCollection {
     private readonly attributesIdsByName = new Map<string, number[]>();
+    private readonly attributesById = new Map<number, AttributeDefinition>();
     private readonly internalRefsAttributes = new Set<number>();
 
     constructor(propertyDatabase: PropertyDatabase, private readonly attributesCaseSensitive: boolean) {
         let nameAttributeId = -1;
         let instanceOfAttributeId = -1;
         propertyDatabase.enumAttributes((attrId, attrDef) => {
-            const attributeName = attributesCaseSensitive 
+            this.attributesById.set(attrId, attrDef);
+
+            const attributeName = attributesCaseSensitive
                 ? attrDef.name
                 : attrDef.name.toLocaleLowerCase();
 
@@ -23,7 +26,7 @@ export class PropertyDatabaseAttributesCollection {
 
             if (attrDef.name === "instanceof_objid" && attrDef.category === "__instanceof__" && attrDef.dataType === 11)
                 instanceOfAttributeId = attrId;
-            
+
             if (attrDef.category === "__internalref__" && attrDef.dataType === 11)
                 this.internalRefsAttributes.add(attrId);
         });
@@ -46,5 +49,9 @@ export class PropertyDatabaseAttributesCollection {
 
     isInternalRefAttribute(attrId: number): boolean {
         return this.internalRefsAttributes.has(attrId);
+    }
+
+    findAttribute(attrId: number): AttributeDefinition | undefined {
+        return this.attributesById.get(attrId);
     }
 }
