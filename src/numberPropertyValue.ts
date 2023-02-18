9a5d69c7582495a368f573ae973160d922b08194
Alexander Ignatovich <cadbimdeveloper@yandex.ru>
2023-02-18 13:17:46 +0200

    high-level code

--- src/numberPropertyValue.ts
+++ src/numberPropertyValue.ts
@@ -7,5 +7,24 @@ export type NumberPropertyValue = {
 };
 
 export const getNumberPropertyValue = (property: NumberPropertyValue, filterSettings: FilterSettings) => {
+    const value = propertyMustBeConverted(property, filterSettings) ? convertValue(property, filterSettings) : property.value;
+    
+    return applyPrecision(value, property.attribute, filterSettings);
+}
+
+const convertValue = (property: NumberPropertyValue, filterSettings: FilterSettings): number => {
+    // TODO
     return property.value;
+}
+
+const applyPrecision = (value: number, attributeDefinition: AttributeDefinition | undefined, filterSettings: FilterSettings): number => {
+    // TODO
+    return value;
+}
+
+const propertyMustBeConverted = (property: NumberPropertyValue, filterSettings: FilterSettings) => {
+    return !!property.attribute
+        && (property.attribute.dataType === 2 || property.attribute.dataType === 3) // int or double
+        && !property.attribute.dataTypeContext
+        && filterSettings.displayUnits !== ""
 }
