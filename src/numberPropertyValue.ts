d0e7f11c6776d2a00d57eb7388ef24305d40801c
Alexander Ignatovich <cadbimdeveloper@yandex.ru>
2023-02-18 14:20:31 +0200

    add unit conversion implementation

--- src/numberPropertyValue.ts
+++ src/numberPropertyValue.ts
@@ -11,7 +11,7 @@ export const getNumberPropertyValue = (property: NumberPropertyValue, filterSett
     applyPrecision(convertValue(property, filterSettings), property.attribute, filterSettings);
 
 const convertValue = (property: NumberPropertyValue, filterSettings: FilterSettings): number => {
-    if (propertyMustBeConverted(property, filterSettings))
+    if (!propertyMustBeConverted(property, filterSettings))
         return property.value;
 
     const unitType = getPropertyUnitType(property.attribute!);
@@ -32,7 +32,7 @@ const applyPrecision = (value: number, attributeDefinition: AttributeDefinition 
 const propertyMustBeConverted = (property: NumberPropertyValue, filterSettings: FilterSettings) => {
     return !!property.attribute
         && (property.attribute.dataType === 2 || property.attribute.dataType === 3) // int or double
-        && !property.attribute.dataTypeContext
+        && !!property.attribute.dataTypeContext
         && filterSettings.displayUnits !== ""
 }
 
@@ -42,6 +42,45 @@ type PropertyUnitType = {
     factor?: number;
 }
 
+type ForgeUnits = {
+    [key: string]: PropertyUnitType;
+}
+
+const forgeUnits: ForgeUnits = {
+    centimeters: { modelUnitType: "CENTIMETER" },
+    cubicCentimeters: { modelUnitType: "CENTIMETER", modificator: "cube" },
+    cubicFeet: { modelUnitType: "FOOT", modificator: "cube" },
+    cubicInches: { modelUnitType: "FOOT", modificator: "cube" },
+    cubicMeters: { modelUnitType: "METER", modificator: "cube" },
+    cubicMillimeters: { modelUnitType: "MILLIMETER", modificator: "cube" },
+    decimeters: { modelUnitType: "METER", factor: 10 },
+    feet: { modelUnitType: "FOOT" },
+    inches: { modelUnitType: "INCH" },
+    kilometers: { modelUnitType: "METER", factor: 1000 },
+    liters: { modelUnitType: "METER", modificator: "cube", factor: 0.001 },
+    meters: { modelUnitType: "METER" },
+    squareCentimeters: { modelUnitType: "CENTIMETER", modificator: "square" },
+    squareFeet: { modelUnitType: "FOOT", modificator: "square" },
+    squareInches: { modelUnitType: "INCH", modificator: "square" },
+    squareMeters: { modelUnitType: "METER", modificator: "square" },
+    squareMillimeters: { modelUnitType: "MILLIMETER", modificator: "square" },
+    feetFractionalInches: { modelUnitType: "FOOT" },
+    fractionalInches: { modelUnitType: "INCH" },
+    metersCentimeters: { modelUnitType: "METER" },
+    stationingFeet: { modelUnitType: "FOOT" },
+    stationingMeters: { modelUnitType: "METER" }
+}
+
 const getPropertyUnitType = (attributeDefinition: AttributeDefinition): PropertyUnitType | null => {
-    return null;
+    const units = attributeDefinition.dataTypeContext;
+
+    if (!units || !units.startsWith("autodesk.unit.unit:"))
+        return null;
+
+    const schemaName = units.split(":")[1];
+    const unitId = schemaName.split("-")[0];
+
+    const unitDef = forgeUnits[unitId];
+
+    return unitDef || null;
 }
---
+++ tests/numberPropertyValue.spec.ts
@@ -0,0 +1,42 @@
+import 'mocha';
+import { assert } from 'chai';
+import { FilterSettings } from '../src/filterSettings';
+import { AttributeDefinition } from '../propertyDatabase';
+import { getNumberPropertyValue } from '../src/numberPropertyValue';
+
+describe("Number conversions tests", () => {
+    const metricAttributeDefinition: AttributeDefinition = {
+        category: "Dimensions",
+        name: "Area",
+        dataType: 3,
+        displayName: "Area",
+        dataTypeContext: "autodesk.unit.unit:squareMeters-1.0.1",
+        precision: 1
+    }
+
+    it("must get original property if units are not specified in filter settings", () => {
+        const filterSettings: FilterSettings = {
+            displayUnits: "",
+            displayUnitsPrecision: "",
+            stringCaseSensitive: true,
+            tolerance: 1e-3
+        };
+
+        const propertyValue = getNumberPropertyValue({ value: 5.7, attribute: metricAttributeDefinition }, filterSettings);
+
+        assert.equal(propertyValue, 5.7);
+    });
+
+    it("must convert to display units", () => {
+        const filterSettings: FilterSettings = {
+            displayUnits: "ft",
+            displayUnitsPrecision: "",
+            stringCaseSensitive: true,
+            tolerance: 1e-3
+        };
+
+        const propertyValue = getNumberPropertyValue({ value: 1, attribute: metricAttributeDefinition }, filterSettings);
+
+        assert.isTrue(Math.abs(propertyValue - 10.76391) < 1e-4);
+    });
+});
