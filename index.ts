6a3c946ffdb47f51f13a895cb26efb7c8ed66cfd
Alexander Ignatovich <cadbimdeveloper@yandex.ru>
2023-02-18 12:01:19 +0200

    simplify query interface

--- index.ts
+++ index.ts
@@ -2,12 +2,12 @@ import { ExpressionComputeResults, QueryResults, Settings, UserComputeOptions, U
 import { IModel } from "./model";
 import { engine } from "./engine";
 
-export async function query(model: IModel, query: string, options: Settings): Promise<QueryResults> {
+export async function query(model: IModel, query: string, options?: Partial<Settings>): Promise<QueryResults> {
   const propertyDatabase = model.getPropertyDb();
 
   const code = `function userFunction(pdb, tag) { const engine = ${engine}; return engine().filterElements(pdb, tag); }`;
 
-  return propertyDatabase.executeUserFunction<QueryResults, UserQueryOptions>(code, { lmvQuery: query, lmvQueryOptions: options });
+  return propertyDatabase.executeUserFunction<QueryResults, UserQueryOptions>(code, { lmvQuery: query, lmvQueryOptions: createFilterSettings(options) });
 }
 
 export async function computeExpressionValue(model: IModel, dbId: number, query: string, attributesCaseSensitive: boolean = true): Promise<ExpressionComputeResults> {
@@ -16,4 +16,27 @@ export async function computeExpressionValue(model: IModel, dbId: number, query:
   const code = `function userFunction(pdb, tag) { const engine = ${engine}; return engine().computeExpression(pdb, tag); }`;
 
   return propertyDatabase.executeUserFunction<ExpressionComputeResults, UserComputeOptions>(code, { nodeId: dbId, propertyQuery: query, caseSensitive: attributesCaseSensitive });
+}
+
+const createFilterSettings = (options?: Partial<Settings>): Settings => {
+  const defaultSettings: Settings = {
+    tolerance: 1e-3,
+    stringCaseSensitive: true,
+    leafNodesOnly: true,
+    attributesCaseSensitive: true,
+    displayUnits: "",
+    displayUnitsPrecision: ""
+  }
+
+  if (!options)
+    return defaultSettings;
+
+  return {
+    tolerance: options.tolerance !== undefined ? options.tolerance : defaultSettings.tolerance,
+    stringCaseSensitive: options.stringCaseSensitive !== undefined ? options.stringCaseSensitive : defaultSettings.stringCaseSensitive,
+    leafNodesOnly: options.leafNodesOnly !== undefined ? options.leafNodesOnly : defaultSettings.leafNodesOnly,
+    attributesCaseSensitive: options.attributesCaseSensitive !== undefined ? options.attributesCaseSensitive : defaultSettings.attributesCaseSensitive,
+    displayUnits: options.displayUnits !== undefined ? options.displayUnits : defaultSettings.displayUnits,
+    displayUnitsPrecision: options.displayUnitsPrecision !== undefined ? options.displayUnitsPrecision : defaultSettings.displayUnitsPrecision
+  }
 }
