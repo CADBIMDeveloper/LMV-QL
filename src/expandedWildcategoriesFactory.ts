243aa98bd21b7c6cf4e734743573ef2c68313ac0
Alexander Ignatovich <cadbimdeveloper@yandex.ru>
2023-02-21 19:58:44 +0200

    extract function

--- src/expandedWildcategoriesFactory.ts
+++ src/expandedWildcategoriesFactory.ts
@@ -8,7 +8,7 @@ export const expandTemplateCategories = (templateCategories: Template, targetLen
 
     expand(templates, [], templateCategories, 0, targetLength);
 
-    return templates.sort((a, b) => computeTemplateScore(a) - computeTemplateScore(b));
+    return templates.sort(compareTemplates);
 }
 
 export const expandTemplateCategoriesForValue = (templateCategories: Template, targetLength: number) => {
@@ -61,5 +61,7 @@ const expand = (templates: Template[], currentTemplate: Template, templateCatego
         expand(templates, currentTemplate, templateCategories, currentIndex + 1, targetLength);
     }
 }
+
+const compareTemplates = (a: Template, b: Template) => computeTemplateScore(a) - computeTemplateScore(b);
 
 const computeTemplateScore = (template: Template): number => template.map((x, i) => x === "*" ? i : 0).reduce((acc, elem) => acc + elem, 0);
