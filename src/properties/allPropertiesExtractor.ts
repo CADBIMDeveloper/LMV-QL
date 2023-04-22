144187bd570e71c2d2614282777f50eefe455dce
Alexander Ignatovich <cadbimdeveloper@yandex.ru>
2023-04-22 13:04:09 +0300

    AllPropertiesExtractor implementation

--- src/properties/allPropertiesExtractor.ts
+++ src/properties/allPropertiesExtractor.ts
@@ -1,21 +1,39 @@
 import { Settings } from "../../output";
+import { getNumberPropertyValue, isNumberProperty } from "../numberPropertyValue";
 import { IQueryableElement } from "../queryableElement";
 import { IQueryPropertiesExtractor, QuryResultRow } from "./queryPropertiesExtractorsFactory";
 
 export class AllPropertiesExtractor implements IQueryPropertiesExtractor {
+    private readonly results: QuryResultRow[] = [];
+    private readonly columns = new Set<string>();
+
     constructor(private readonly settings: Settings) {
-        
+
     }
 
     push(dbId: number, element: IQueryableElement): void {
-        throw new Error("Method not implemented.");
+        const rowValue: QuryResultRow = {
+            dbIds: [dbId],
+            values: {}
+        }
+
+        const propertyValues = element.getObjectProperties();
+
+        for (const property of propertyValues) {
+            if (!property.attribute)
+                continue;
+
+            rowValue.values[property.attribute.name] = isNumberProperty(property) ? getNumberPropertyValue(property, this.settings) : property.value;
+        }
+
+        this.results.push(rowValue);
     }
-    
+
     compile(): QuryResultRow[] {
-        throw new Error("Method not implemented.");
+        return this.results;
     }
-    
+
     getColumns(): string[] {
-        throw new Error("Method not implemented.");
+        return Array.from(this.columns.values());
     }
 }
