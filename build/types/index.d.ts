7ea25b15545e334376941cc9d03d093a69631e63
Alexander Ignatovich <cadbimdeveloper@yandex.ru>
2023-11-16 23:04:11 +0200

    use types from decs.d.ts

--- build/types/index.d.ts
+++ build/types/index.d.ts
@@ -1,5 +1,4 @@
 import { ComputeSettings, ExpressionComputeResults, QueryResults, Settings } from "./output";
-import { IBubbleNode, IDocumentNode, IModel } from "./model";
-export declare function query(model: IModel, query: string, options?: Partial<Settings>): Promise<QueryResults>;
-export declare function headlessQuery(viewerDocument: IDocumentNode, bubbleNode: IBubbleNode, queryString: string, options?: Partial<Settings>): Promise<QueryResults>;
-export declare function computeExpressionValue(model: IModel, dbId: number, queryString: string, options?: Partial<ComputeSettings>): Promise<ExpressionComputeResults>;
+export declare function query(model: Autodesk.Viewing.Model, query: string, options?: Partial<Settings>): Promise<QueryResults>;
+export declare function headlessQuery(viewerDocument: Autodesk.Viewing.Document, bubbleNode: Autodesk.Viewing.BubbleNode, queryString: string, options?: Partial<Settings>): Promise<QueryResults>;
+export declare function computeExpressionValue(model: Autodesk.Viewing.Model, dbId: number, queryString: string, options?: Partial<ComputeSettings>): Promise<ExpressionComputeResults>;
