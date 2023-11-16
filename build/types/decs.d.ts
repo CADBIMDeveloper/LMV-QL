ec7b0c0d532c808d9baf82eeb0e14eb3a7eb2d4b
Alexander Ignatovich <cadbimdeveloper@yandex.ru>
2023-11-16 23:13:20 +0200

    copy decs.d.ts into package types

---
+++ build/types/decs.d.ts
@@ -0,0 +1,41 @@
+declare namespace Autodesk {
+    namespace Viewing {
+        const OBJECT_TREE_CREATED_EVENT = 'objectTreeCreated';
+
+        class Model {
+            constructor(data: object);
+            getInstanceTree(): InstanceTree;
+            getPropertyDb(): Private.PropDbLoader;
+        }
+
+        class InstanceTree {
+            getRootId(): number;
+
+            enumNodeChildren(node: number, callback: (dbId: number) => void | boolean, recursive?: boolean): void
+        }
+
+        class EventDispatcher {
+            addEventListener(type: string, listener: (event: any) => void, options?: any): void;
+        }
+
+        class BubbleNode {
+            isSVF2(): boolean;
+            findPropertyDbPath(): string;
+        }
+
+        class Document {
+            getFullPath(urn: string): string;
+            getAcmSessionId(url: string): string | undefined;
+        }
+
+        namespace Private {
+            class PropDbLoader {
+                constructor(propertyDbPath: string, model: Model, eventDispatcher?: EventDispatcher);
+
+                load(): void;
+
+                executeUserFunction<TResult, TOptions>(code: ((pdb: any, tag?: TOptions) => TResult) | string, tag?: TOptions): Promise<TResult>;
+            }
+        }
+    }
+}
